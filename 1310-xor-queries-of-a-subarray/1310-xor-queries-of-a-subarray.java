14326d5d0fb69c3a98e36379c47d41386e38efa8
Anshul <anshulrana772@gmail.com>
2024-09-13 20:55:26 +0545

    Time: 2 ms (100.00%), Space: 57.6 MB (55.23%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -1,15 +1,15 @@
 public class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        // Step 1: Compute the prefix XOR array
+      
         int n = arr.length;
-        int[] prefixXOR = new int[n + 1]; // Extra space for easier indexing
+        int[] prefixXOR = new int[n + 1]; 
         
-        // Compute prefixXOR such that prefixXOR[i] = arr[0] ^ arr[1] ^ ... ^ arr[i-1]
+        
         for (int i = 1; i <= n; i++) {
             prefixXOR[i] = prefixXOR[i - 1] ^ arr[i - 1];
         }
 
-        // Step 2: Answer each query using the prefixXOR array
+    
         int[] result = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
             int left = queries[i][0];
