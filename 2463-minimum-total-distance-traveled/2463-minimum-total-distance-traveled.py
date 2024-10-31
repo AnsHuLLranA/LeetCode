19976a0dc0b4d0b302d989c92522fd34daba67cd
Anshul <anshulrana772@gmail.com>
2024-10-31 21:36:58 +0530

    Time: 1054 ms (80.00%), Space: 17.1 MB (81.63%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
@@ -6,17 +6,21 @@ class Solution:
         n = len(robot)
         m = len(factory)
         
-        # Initialize DP table with "infinity" values
+       
+    
         dp = [[float('inf')] * (n + 1) for _ in range(m + 1)]
-        dp[0][0] = 0  # No distance if no robots and no factories used
+        dp[0][0] = 0  
+        
 
         for i in range(1, m + 1):
-            dp[i][0] = 0  # No robots to repair means 0 distance for any factories
+            dp[i][0] = 0  
+            
             for j in range(1, n + 1):
-                dp[i][j] = dp[i - 1][j]  # Option of not using this factory
+                dp[i][j] = dp[i - 1][j]  
+                
                 distance = 0
                 
-                # Calculate the cumulative distance for possible repairs at this factory
+
                 for k in range(1, min(j, factory[i - 1][1]) + 1):
                     distance += abs(factory[i - 1][0] - robot[j - k])
                     if dp[i - 1][j - k] != float('inf'):
